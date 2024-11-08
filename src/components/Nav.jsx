1394d01a0fd1159c1ba7abc794d8066a4b57a49e
Youssef-ahim <youssefahkim1234@gmail.com>
2024-11-08 14:12:39 +0100

    third commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,16 +5,21 @@ export default function Nav({ onSectionChange }) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <nav className="shadow-2xl w-[90%] flex justify-between items-center p-4 bg-opacity-90 rounded-lg nav-fade-in">
+        <nav className="shadow-2xl w-[90%] mx-auto flex justify-between items-center p-4 bg-opacity-90 rounded-lg nav-fade-in relative z-10">
             {/* Logo */}
             <img src={logo} className="logo w-10 sm:w-12 logo-slide-in" alt="logo" />
 
             {/* Desktop Menu */}
-            <ul className="hidden sm:flex justify-around items-center w-full max-w-lg text-white text-lg font-serif font-bold">
-                <button onClick={() => onSectionChange('home')} className="nav-item nav-item-fade-up hover:text-blue-400 transition duration-300">Home</button>
-                <button onClick={() => onSectionChange('about')} className="nav-item nav-item-fade-up hover:text-blue-400 transition duration-300">About</button>
-                <button onClick={() => onSectionChange('skills')} className="nav-item nav-item-fade-up hover:text-blue-400 transition duration-300">Skills</button>
-                <button onClick={() => onSectionChange('projects')} className="nav-item nav-item-fade-up hover:text-blue-400 transition duration-300">Projects</button>
+            <ul className="hidden sm:flex items-center justify-around w-full max-w-lg text-white text-lg font-serif font-bold">
+                {['Home', 'About', 'Skills', 'Projects'].map((section) => (
+                    <button
+                        key={section}
+                        onClick={() => onSectionChange(section.toLowerCase())}
+                        className="nav-item nav-item-fade-up hover:text-blue-400 transition duration-300"
+                    >
+                        {section}
+                    </button>
+                ))}
             </ul>
 
             {/* Mobile Menu Toggle */}
@@ -28,36 +33,29 @@ export default function Nav({ onSectionChange }) {
 
             {/* Mobile Menu */}
             {isMenuOpen && (
-                <ul className="sm:hidden absolute  right-[10%] top-0 w-auto z-50 bg-opacity-95  text-white text-lg font-serif font-bold flex flex-col items-center py-4 rounded-b-lg shadow-md duration-300">
-                    <div className='flex justify-between'>
-                        <button onClick={() => { setIsMenuOpen(false); onSectionChange('home'); }} className="nav-item nav-item-fade-up hover:text-blue-400 py-2  duration-300">Home</button>
-                        <button onClick={() => { setIsMenuOpen(false); onSectionChange('about'); }} className="nav-item nav-item-fade-up hover:text-blue-400 py-2  duration-300">About</button>
-                    </div>
-
-                    <div className='flex justify-between w-[100%]'>
-                        <button onClick={() => { setIsMenuOpen(false); onSectionChange('skills'); }} className="nav-item nav-item-fade-up hover:text-blue-400 py-2  duration-300">Skills</button>
-                        <button onClick={() => { setIsMenuOpen(false); onSectionChange('projects'); }} className="nav-item nav-item-fade-up hover:text-blue-400 py-2  duration-300">Projects</button>
-                    </div>
+                <ul className="px-[10%] sm:hidden absolute right-[5%] top-16 w-[70%] bg-gray-900 bg-opacity-95 text-white text-lg font-serif font-bold flex flex-col items-center py-4 rounded-lg shadow-lg">
+                    {['Home', 'About', 'Skills', 'Projects'].map((section) => (
+                        <button
+                            key={section}
+                            onClick={() => { setIsMenuOpen(false); onSectionChange(section.toLowerCase()); }}
+                            className=" nav-item nav-item-fade-up hover:text-blue-400 py-2 transition duration-300  text-center"
+                        >
+                            {section}
+                        </button>
+                    ))}
                 </ul>
             )}
 
-
             {/* Hire Me Button */}
             <button
-                onClick={() => {
-                    window.open(
-                        "https://mail.google.com/mail/?view=cm&fs=1&to=youssefahkim1234@gmail.com&su=Hire%20Me%20Request&body=Hello%20Youssef,%20I'm%20interested%20in%20working%20with%20you.",
-                        "_blank"
-                    );
-                }}
+                onClick={() => window.open(
+                    "https://mail.google.com/mail/?view=cm&fs=1&to=youssefahkim1234@gmail.com&su=Hire%20Me%20Request&body=Hello%20Youssef,%20I'm%20interested%20in%20working%20with%20you.",
+                    "_blank"
+                )}
                 className="hire-button hidden sm:inline-block bg-blue-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-600 duration-300"
             >
                 Hire Me
             </button>
-
-
-
-
         </nav>
     );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,26 +3,37 @@ import image1 from "../img/projet-image-1.png";
 
 const projectsData = [
   { image: image1, title: "this portfolio" },
+  { image: image1, title: "this portfolio" },
+  { image: image1, title: "this portfolio" },
+
 
 ];
 
 export default function Projects() {
   return (
-    <div className="projects   max-w-[90%] md:max-w-[70%] lg:max-w-[55%] max-h-[60vh]  overflow-y-scroll ml-[5%] mr-auto mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 hide-scrollbar rounded-2xl p-[1%]">
-      {projectsData.map((project, index) => (
-        <div
-          key={index}
-          className=" project-item flex flex-col justify-between items-center w-full h-40 shadow-xl shadow-blue-500/50 rounded-2xl transition-transform transform hover:scale-105 "
-        >
-          <div className=" bg-white w-full flex justify-center items-center rounded-t-2xl text-lg font-semibold text-gray-800">
-            <img src={project.image} alt={project.image} className="h-full  w-full w-100vw object-cover rounded-t-2xl" />
-          </div>
-
-          <h2 className="h-[30%] flex justify-center items-center bg-slate-500 w-full rounded-b-2xl text-white font-medium p-2 text-sm">
-            {project.title}
-          </h2>
+    <div className="projects max-w-[90%] md:max-w-[70%] lg:max-w-[55%] max-h-[70vh] overflow-y-scroll ml-[5%] mr-auto mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 hide-scrollbar rounded-2xl p-4">
+    {projectsData.map((project, index) => (
+      <div
+        key={index}
+        className="project-item flex flex-col justify-between items-center w-full h-full shadow-xl shadow-blue-500/50 rounded-2xl transition-transform transform hover:scale-105 duration-300"
+      >
+        {/* Image Container */}
+        <div className="w-full bg-white flex justify-center items-center rounded-t-2xl overflow-hidden aspect-w-16 aspect-h-9 sm:aspect-w-16 sm:aspect-h-9 md:aspect-w-4 md:aspect-h-3 lg:aspect-w-3 lg:aspect-h-2">
+          <img
+            src={project.image}
+            alt={project.title}
+            className="w-full h-full object-contain"
+          />
         </div>
-      ))}
-    </div>
+  
+        {/* Project Title */}
+        <h2 className="w-full bg-slate-500 text-white text-center font-medium text-sm p-3 rounded-b-2xl">
+          {project.title}
+        </h2>
+      </div>
+    ))}
+  </div>
+  
+
   );
 }
