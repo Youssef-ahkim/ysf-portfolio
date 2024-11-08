5c461c6e6f35a2b3cacc14467aac48328ba8d784
Youssef-ahim <youssefahkim1234@gmail.com>
2024-11-08 14:31:24 +0100

    fourt commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function About() {
     return (
-        <section className="  hero-section flex flex-col justify-center items-start min-h-[60vh] px-4 sm:px-8 lg:px-16 z-1 ">
-            <div className="about-text max-w-full sm:max-w-[80%] lg:max-w-[50%] bg-gradient-to-br from-blue-900 via-blue-70000  text-white border border-white/30 p-6 rounded-2xl shadow-xl transition-transform duration-300 transform hover:scale-105 hover:shadow-2xl">
+        <section className="hero-section flex flex-col justify-center items-start min-h-[60vh] px-4 sm:px-8 lg:px-16 z-1">
+            <div className="about-text max-w-full sm:max-w-[80%] lg:max-w-[50%] bg-gradient-to-br from-blue-700 via-blue-900 to-gray-800 text-white border border-white/30 p-6 rounded-2xl shadow-xl transition-transform duration-300 transform hover:scale-105 hover:shadow-2xl backdrop-blur-lg">
                 <h1 className="text-3xl sm:text-4xl lg:text-5xl font-semibold mb-4 text-cyan-100">
                     Youssef Ahkim
                 </h1>
